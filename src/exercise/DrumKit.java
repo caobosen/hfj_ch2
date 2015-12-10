c047e3d68d4461af9de91cc9bba2703316022b76
caoyaxin <caobosen@hotmail.com>
2015-12-10 12:40:27 -0500

    exercise of ch2 done

---
+++ src/exercise/DrumKit.java
@@ -0,0 +1,16 @@
+package exercise;
+
+public class DrumKit {
+
+	boolean topHat = true;
+	boolean snare = true;
+
+	void playSnare() {
+		System.out.println("Bang bang ba-bang!!!!!!!!!");
+	}
+
+	void playTopHat() {
+		System.out.println("Ding ding da-ding!");
+	}
+
+}
